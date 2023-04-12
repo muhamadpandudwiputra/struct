20086f3c2b746d64ce0c5109a9557fb50d75e6be
muhamadpandudwiputra <112594630+muhamadpandudwiputra@users.noreply.github.com>
2023-04-12 11:45:05 +0700

    Add files via upload

---
+++ STRUCT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+struct mahasiswa{
+	string nama, npm;
+};
+
+int main(){
+	mahasiswa mhs;
+	cin >> mhs.nama;
+	cin >> mhs.npm;
+	
+	cout << mhs.nama <<endl;
+	cout << mhs.npm;
+}
